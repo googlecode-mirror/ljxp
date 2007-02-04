6e877d54b25325d00fe64d6a85dd771d788af24d
evan.broder <evan.broder@b7b29716-9f1a-0410-98de-79dcf2039bee>
2007-02-04 20:22:36 +0000

    v1.2 to 1.0 branch

--- lj_crosspost.php
+++ lj_crosspost.php
@@ -3,7 +3,7 @@
 Plugin Name: LiveJournal Crossposter
 Plugin URI: http://ebroder.net/livejournal-crossposter/
 Description: Automatically copies all posts to a LiveJournal or other LiveJournal-based blog. Editing or deleting a post will be replicated as well. This plugin was inspired by <a href="http://blog.mytechaid.com/">Scott Buchanan's</a> <a href="http://blog.mytechaid.com/archives/2005/01/10/xanga-crossposter/">Xanga Crossposter</a>
-Version: 1.1
+Version: 1.2
 Author: Evan Broder
 Author URI: http://ebroder.net/
 
@@ -45,6 +45,7 @@ function ljxp_display_options() {
 	add_option('ljxp_custom_name_on');
 	add_option('ljxp_custom_name');
 	add_option('ljxp_privacy');
+	add_option('ljxp_comments');
 	
 	// Retrieve these for the form
 	$old_host = get_option('ljxp_host');
@@ -52,6 +53,7 @@ function ljxp_display_options() {
 	$old_name = get_option('ljxp_custom_name');
 	$old_name_on = get_option('ljxp_custom_name_on');
 	$old_privacy = get_option('ljxp_privacy');
+	$old_comments = get_option('ljxp_comments');
 	
 	// host should default to LJ - it's what most people use anyway
 	if("" == $old_host) {
@@ -72,6 +74,12 @@ function ljxp_display_options() {
 	if("" == $old_privacy) {
 		update_option('ljxp_privacy', 'public');
 		$old_privacy = "public";
+	}
+	
+	// Defaulting to no comments on LJ - makes more sense
+	if("" == $old_comments) {
+		update_option('ljxp_comments', '0');
+		$old_comments = '0';
 	}
 	
 	// If we're handling a submission, save the data
@@ -103,6 +111,11 @@ function ljxp_display_options() {
 			$old_privacy = $_REQUEST['privacy'];
 		}
 		
+		if($old_comments != $_REQUEST['comments']) {
+			update_option('ljxp_comments', $_REQUEST['comments']);
+			$old_comments = $_REQUEST['comments'];
+		}
+		
 		// If a password value is entered, md5 it for security and store to the
 		// database
 		// LJ challenge authentication works with only knowing the md5 of the
@@ -112,7 +125,7 @@ function ljxp_display_options() {
 		}
 		
 		// Copied from another options page
-		echo '<div class="updated"><p><strong>Options saved.</strong></p></div>';
+		echo '<div id="message" class="updated fade"><p><strong>Options saved.</strong></p></div>';
 	}
 	
 	// And, finally, output the form
@@ -188,6 +201,24 @@ function ljxp_display_options() {
 				</tr>
 			</table>
 		</fieldset>
+		<fieldset class="options">
+			<legend>LiveJournal Comments</legend>
+			<table width="100%" cellspacing="2" cellpadding="5" class="editform">
+				<tr valign="top">
+					<th width="33%" scope="row">Should comments be allowed on LiveJournal?</th>
+					<td><label><input name="comments" type="radio" value="0" <?php
+					if(0 == $old_comments) {
+						echo 'checked="checked" ';
+					}
+					?>/> Require users to comment on WordPress</label><br />
+					<label><input name="comments" type="radio" value="1" <?php
+					if("1" == $old_comments) {
+						echo 'checked="checked" ';
+					}
+					?>/> Allow comments on LiveJournal</label><br />
+				</tr>
+			</table>
+		</fieldset>
 		<p class="submit">
 			<input type="submit" name="update_lj_options" value="Save Options &raquo;" />
 		</p>
@@ -206,6 +237,7 @@ function ljxp_post($post_id) {
 	$custom_name_on = get_option('ljxp_custom_name_on');
 	$custom_name = get_option('ljxp_custom_name');
 	$privacy = get_option('ljxp_privacy');
+	$comments = get_option('ljxp_comments');
 	
 	// And create our connection
 	$client = new IXR_Client($host, '/interface/xmlrpc');
@@ -223,25 +255,72 @@ function ljxp_post($post_id) {
 	$response = $client->getResponse();
 	$challenge = $response[challenge];
 	
-	// Same header as Xanga
-	$postHeader = '<p style="border: 1px solid black; padding: 3px;"><b>Originally published at <a href="'.
-		get_permalink($post_id).'">';
+	$post = & get_post($post_id);
 	
-	// Insert the name of the page we're linking back to based on the options set
-	if(!$custom_name_on) {
-		$postHeader .= get_option("blogname");
+	// If the post is not password protected, follow standard procedure
+	if(!$post->post_password) {
+		$postHeader = '<p style="border: 1px solid black; padding: 3px;"><b>Originally published at <a href="'.
+			get_permalink($post_id).'">';
+
+		// Insert the name of the page we're linking back to based on the options set
+		if(!$custom_name_on) {
+			$postHeader .= get_option("blogname");
+		}
+		else {
+			$postHeader .= $custom_name;
+		}
+
+		$postHeader .= '</a>.';
+	}
+	// If the post is password protected, put up a special message
+	else {
+		$postHeader = '<p style="border: 1px solid black; padding: 3px;"><b>This post is password protected. You can read it at <a href="'.
+			get_permalink($post_id).'">';
+
+		// Insert the name of the page we're linking back to based on the options set
+		if(!$custom_name_on) {
+			$postHeader .= get_option("blogname");
+		}
+		else {
+			$postHeader .= $custom_name;
+		}
+
+		$postHeader .= '</a>, where it was originally posted.';
+	}
+	
+	// Depending on whether comments or allowed or not, alter the header
+	// appropriately
+	if($comments) {
+		$postHeader .= ' You can comment here or <a href="'.get_permalink($post_id).
+			'#comments">there</a>.</b></p>';
 	}
 	else {
-		$postHeader .= $custom_name;
+		$postHeader .= ' Please leave any <a href="'.get_permalink($post_id).
+			'#comments">comments</a> there.</b></p>';
 	}
 	
-	$postHeader .= '</a>.  Please leave any <a href="'.get_permalink($post_id).
-		'#comments">comments</a> there.</b></p>';
+	// $the_event will eventually be passed to the LJ XML-RPC server. In all
+	// cases, we want whatever header we put together up above
+	$the_event = $postHeader;
 	
-	// Bypassing the get_post() function because it caches and teh cache isn't
-	// updated when the entry is. A noble, query-saving idea, but not so hot
-	// in the implimentation, really
-	$post = & get_post($post_id);
+	// and if the post isn't password protected, we want the actual post too
+	if(!$post->post_password) {
+		$the_event .= $post->post_content;
+	}
+	
+	// Retrieve the categories that the post is marked as - for LJ tagging
+	$cats = wp_get_post_cats('', $post_id);
+	// I need them in an array for my next trick to work
+	if(!is_array($cats)) {
+		$cats = array($cats);
+	}
+	// Convert the category IDs of all categories to their text names
+	$cat_names = array_map("get_cat_name", $cats);
+	// Turn them into a comma-seperated list for the API
+	$cat_string = implode(", ", $cat_names);
+	
+	// Get the most recent post (to see if this is it - it it's not, backdate)
+	$recent_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_date_gmt <= '$now' AND post_status = 'publish' ORDER BY post_date_gmt DESC LIMIT 1");
 	
 	// Get a timestamp for retrieving dates later
 	$date = strtotime($post->post_date);
@@ -254,9 +333,9 @@ function ljxp_post($post_id) {
 	// md5(challenge + md5(pwd))
 	$args[auth_response] = md5($challenge . $pass);
 	
-	// The header + the actual post
-	$args[event] = $postHeader . $post->post_content;
-	$args[subject] = $post->post_title;
+	// The filters run the WP texturization - cleans up the code
+	$args[event] = mb_convert_encoding(apply_filters('the_content', $the_event), "HTML-ENTITIES", "UTF-8");
+	$args[subject] = mb_convert_encoding(apply_filters('the_title', $post->post_title), "HTML-ENTITIES", "UTF-8");
 	
 	// All of the relevent dates and times
 	$args[year] = date('Y', $date);
@@ -264,7 +343,19 @@ function ljxp_post($post_id) {
 	$args[day] = date('j', $date);
 	$args[hour] = date('G', $date);
 	$args[min] = date('i', $date);
-	$args[props] = array("opt_nocomments" => true);
+						// Enable or disable comments as specified by the
+						// settings
+	$args[props] = array("opt_nocomments" => !$comments, 
+						// Tells LJ to not run it's formatting (replacing \n
+						// with <br>, etc) because it's already been done by
+						// the texturization
+						"opt_preformatted" => true,
+						// Set tags
+						"taglist" => $cat_string,
+						// If the most recent post is not the one being dealt
+						// with now, mark it as backdated so it doesn't jump to
+						// the top of friendlists and such
+						"opt_backdated" => !($post_id == $recent_id));
 	
 	// Set the privacy level according to the settings
 	switch($privacy) {
@@ -300,7 +391,6 @@ function ljxp_post($post_id) {
 		// Store it to the metadata
 		add_post_meta($post_id, 'ljID', $response[itemid]);
 	}
-	
 	// If you don't return this, other plugins and hooks won't work
 	return $post_id;
 }
@@ -358,12 +448,39 @@ function ljxp_delete($post_id) {
 		die('Something went wrong - '.$client->getErrorCode().' : '.$client->getErrorMessage());
 	}
 	
+	delete_post_meta($post_id, 'ljID');
+	
+	return $post_id;
+}
+
+function ljxp_edit($post_id) {
+	// This function will delete a post from LJ if it's changed from the
+	// published status
+	
+	// Pull the post_id
+	$ljxp_post_id = get_post_meta($post_id, 'ljID', true);
+	
+	// Ensures that there's actually a value. If the post was never
+	// cross-posted, the value wouldn't be set, so we're done
+	if(0 == $ljxp_post_id) {
+		return;
+	}
+	
+	$post = & get_post($post_id);
+	
+	// See if the post is currently published. If it's been crossposted and its
+	// state isn't published, then it should be deleted
+	if('publish' != $post->post_status) {
+		ljxp_delete($post_id);
+	}
+	
 	return $post_id;
 }
 
 add_action('admin_menu', 'ljxp_add_pages');
 if(get_option('ljxp_username') != "") {
 	add_action('publish_post', 'ljxp_post', 8);
+	add_action('edit_post', 'ljxp_edit', 8);
 	add_action('delete_post', 'ljxp_delete', 8);
 }
 
